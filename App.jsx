712a754929fd81df4c38d945d062cfc8d49a6a3f
marcterradas <terradas.marc@gmail.com>
2022-04-16 20:29:29 +0200

    js to jsx

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native'
-
 import Footer from './components/Footer'
 
 function App() {
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { View, Text } from 'react-native'
 
