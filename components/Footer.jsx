f0981fd27f430686d25375b17c62e23194c9e5e3
marcterradas <terradas.marc@gmail.com>
2022-04-17 09:04:50 +0200

    add icons bottom navigation

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 import { View, Text } from "react-native";
 
 const Tab = createBottomTabNavigator();
@@ -22,7 +23,28 @@ function SettingsScreen() {
 
 function Footer() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          switch (route.name) {
+            case "Home":
+              iconName = focused
+                ? "information-circle"
+                : "information-circle-outline";
+              break;
+            case "Settings":
+              iconName = focused ? "settings" : "settings-outline";
+              break;
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
     </Tab.Navigator>
