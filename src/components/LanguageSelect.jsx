21f971a3f781853bbbc4fc315547f5e79a1b4a3e
marcterradas <terradas.marc@gmail.com>
2022-04-18 13:33:12 +0200

    styles documents

--- src/components/LanguageSelect.jsx
+++ src/components/LanguageSelect.jsx
@@ -23,6 +23,7 @@ const LanguageSelect = () => {
       backgroundColor: "#fff",
     },
   };
+
   function updateLanguage(lanugage) {
     changeLanguage(lanugage);
     setSelectedLanguage(lanugage);
