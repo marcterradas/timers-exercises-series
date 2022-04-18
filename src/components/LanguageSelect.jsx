1066f4ad13e4b710d8ddae05d20f1b5a2686089b
marcterradas <terradas.marc@gmail.com>
2022-04-18 13:27:37 +0200

    create LanguageSelect component

---
+++ src/components/LanguageSelect.jsx
@@ -0,0 +1,42 @@
+import { React, useState } from "react";
+import { Picker } from "@react-native-picker/picker";
+import {
+  languages,
+  changeLanguage,
+  defaultLanguage,
+} from "../../languages/i18n";
+
+const LanguageSelect = () => {
+  const [selectedLanguage, setSelectedLanguage] = useState(defaultLanguage);
+  const languagesElements = languages.map((language) => {
+    return (
+      <Picker.Item
+        key={language.key}
+        label={language.value}
+        value={language.key}
+      />
+    );
+  });
+  const styles = {
+    select: {
+      marginTop: 10,
+      backgroundColor: "#fff",
+    },
+  };
+  function updateLanguage(lanugage) {
+    changeLanguage(lanugage);
+    setSelectedLanguage(lanugage);
+  }
+
+  return (
+    <Picker
+      style={styles.select}
+      selectedValue={selectedLanguage}
+      onValueChange={(itemValue) => updateLanguage(itemValue)}
+    >
+      {languagesElements}
+    </Picker>
+  );
+};
+
+export default LanguageSelect;
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,51 +1,21 @@
-import { React, useState } from "react";
-import { Picker } from "@react-native-picker/picker";
+import { React } from "react";
 import { View, Text } from "react-native";
 import { useTranslation } from "react-i18next";
-import {
-  languages,
-  changeLanguage,
-  defaultLanguage,
-} from "../../languages/i18n";
+import LanguageSelect from "../components/LanguageSelect";
 
 const SettingsScreen = () => {
-  const [selectedLanguage, setSelectedLanguage] = useState(defaultLanguage);
-  const languagesElements = languages.map((language) => {
-    return (
-      <Picker.Item
-        key={language.key}
-        label={language.value}
-        value={language.key}
-      />
-    );
-  });
   const { t } = useTranslation();
 
   const styles = {
     container: {
       padding: 10,
     },
-    select: {
-      marginTop: 10,
-      backgroundColor: "#fff",
-    },
   };
-
-  function updateLanguage(lanugage) {
-    changeLanguage(lanugage);
-    setSelectedLanguage(lanugage);
-  }
 
   return (
     <View style={styles.container}>
       <Text>{t("change_language")}</Text>
-      <Picker
-        style={styles.select}
-        selectedValue={selectedLanguage}
-        onValueChange={(itemValue) => updateLanguage(itemValue)}
-      >
-        {languagesElements}
-      </Picker>
+      <LanguageSelect />
     </View>
   );
 };
