f846cd6444e43228def4bb8a3535973dbd30ff2b
marcterradas <terradas.marc@gmail.com>
2022-04-20 19:42:05 +0200

    init action button on workout page

--- src/pages/Workout.jsx
+++ src/pages/Workout.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { View, Text } from "react-native";
+import ActionButton from "react-native-action-button";
 
 const WorkoutScreen = () => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Workout Screen ...</Text>
+      <ActionButton
+        buttonColor="rgba(231,76,60,1)"
+        onPress={() => {
+          console.log("hi");
+        }}
+      />
     </View>
   );
 };
