df0ff9ad8b959e8aa910811979fca77346fd7c27
marcterradas <terradas.marc@gmail.com>
2022-04-23 09:54:15 +0200

    init workout action button logic

--- src/pages/Workout.jsx
+++ src/pages/Workout.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import { React, useState } from "react";
 import { View, Text } from "react-native";
 import ActionButton from "react-native-action-button";
 
 const WorkoutScreen = () => {
+  const actionButtonTemplate = (
+    <ActionButton buttonColor="black" onPress={createWorkout} />
+  );
+  const [actionButton, setShowActionButton] = useState(actionButtonTemplate);
 
-  const createWorkout = () => {
-    console.log("create workout");
-  };
+  function createWorkout() {
+    // setShowActionButton(false);
+  }
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Workout Screen ...</Text>
-      <ActionButton buttonColor="black" onPress={createWorkout} />
+      {actionButton}
     </View>
   );
 };
