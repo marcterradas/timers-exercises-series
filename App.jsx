e9f39f2b4583a8642f0df5796b65f0a506e06121
marcterradas <terradas.marc@gmail.com>
2022-04-23 15:38:42 +0200

    remove log useNativeDriver

--- App.jsx
+++ App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-
+import { LogBox } from "react-native";
 import "./languages/i18n";
-
 import Footer from "./src/layout/Footer";
+
+LogBox.ignoreLogs(["Animated: `useNativeDriver`"]);
 
 const App = () => {
   return (
