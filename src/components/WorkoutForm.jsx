79363062f620b4a3d69b00379a966351670c8221
marcterradas <terradas.marc@gmail.com>
2022-05-15 12:46:40 +0200

    feat: :sparkles: workout name add label

--- src/components/WorkoutForm.jsx
+++ src/components/WorkoutForm.jsx
@@ -6,6 +6,7 @@ const WorkoutForm = (t) => {
   return (
     <View style={styles.form}>
       <Text style={styles.title}>{t("new_workout")}</Text>
+      <Text>{t("workout_name")}</Text>
       <TextInput style={styles.input}></TextInput>
     </View>
   );
