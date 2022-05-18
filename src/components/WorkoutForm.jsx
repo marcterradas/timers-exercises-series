9551c7827ccf974fb2236c03e5f5c7389250ea47
marcterradas <terradas.marc@gmail.com>
2022-05-18 21:05:12 +0200

    feat: :sparkles: init logic

--- src/components/WorkoutForm.jsx
+++ src/components/WorkoutForm.jsx
@@ -1,4 +1,4 @@
-import { React } from "react";
+import { React, useState } from "react";
 import { View, Text, TextInput, Button } from "react-native";
 import { useTranslation } from "react-i18next";
 
@@ -7,6 +7,7 @@ import { saveWorkout } from "../logic/workout";
 
 const WorkoutForm = () => {
   const { t } = useTranslation();
+  const [title, setTitle] = useState("");
 
   return (
     <View style={styles.form}>
@@ -15,11 +16,15 @@ const WorkoutForm = () => {
       </View>
       <View>
         <Text>{t("workout_name")}</Text>
-        <TextInput style={styles.input}></TextInput>
+        <TextInput
+          style={styles.input}
+          value={title}
+          onChangeText={setTitle}
+        ></TextInput>
       </View>
       <View style={styles.actionContainer}>
         <Button
-          onPress={saveWorkout()}
+          onPress={saveWorkout(title)}
           style={styles.button}
           title={t("save")}
           accessibilityLabel={t("save")}
