d21b61adf34a0115b73272ea5bb39313b48405ed
marcterradas <terradas.marc@gmail.com>
2022-05-24 18:21:54 +0200

    feat: :art: update action button label

--- src/pages/workout/Main.jsx
+++ src/pages/workout/Main.jsx
@@ -1,5 +1,5 @@
 import { React, useState } from "react";
-import { View, ScrollView, Button } from "react-native";
+import { View, ScrollView } from "react-native";
 import ActionButton from "../../components/ActionButton";
 import List from "./List";
 import Form from "./Form";
@@ -8,15 +8,17 @@ import { t } from "i18next";
 
 const WorkoutScreen = () => {
   const [page, setPage] = useState("list");
+  const [actionLabel, setActionLabel] = useState(t("workout_add"));
 
   function changePage() {
     setPage(page === "list" ? "form" : "list");
+    setActionLabel(page === "list" ? t("workout_list") : t("workout_add"));
   }
 
   return (
     <View style={styles.container}>
       <ScrollView>{page == "list" ? <List /> : <Form />}</ScrollView>
-      <ActionButton label={t("change_page")} method={changePage} />
+      <ActionButton label={actionLabel} method={changePage} />
     </View>
   );
 };
