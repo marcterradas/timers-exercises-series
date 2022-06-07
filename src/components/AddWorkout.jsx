5be10ee6188d17bb22eafc3def5c3ad35211de15
marcterradas <terradas.marc@gmail.com>
2022-06-07 20:24:13 +0200

    init logic add workout

--- src/components/AddWorkout.jsx
+++ src/components/AddWorkout.jsx
@@ -1,11 +1,13 @@
-import { React } from "react";
-import { Text, TouchableOpacity } from "react-native";
+import { React, useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
 import IonIcon from "react-native-vector-icons/Ionicons";
 import { t } from "i18next";
 
 import styles from "../styles/workout.styles";
 
 const AddWorkout = () => {
+  const [showForm, setShowForm] = useState(false);
+
   const icon = {
     name: "add-circle-outline",
     size: 40,
@@ -13,15 +15,27 @@ const AddWorkout = () => {
   };
 
   function addExercice() {
-    console.log("add exercice");
+    setShowForm(true);
   }
 
-  return (
-    <TouchableOpacity onPress={addExercice} style={styles.bigButton}>
-      <IonIcon name={icon.name} size={icon.size} color={icon.color} />
-      <Text style={styles.buttonText}>{t("add")}</Text>
-    </TouchableOpacity>
-  );
+  const Button = () => {
+    return (
+      <TouchableOpacity onPress={addExercice} style={styles.bigButton}>
+        <IonIcon name={icon.name} size={icon.size} color={icon.color} />
+        <Text style={styles.buttonText}>{t("add")}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  const Form = () => {
+    return (
+      <View>
+        <Text>{1111}</Text>
+      </View>
+    );
+  };
+
+  return showForm ? <Form /> : <Button />;
 };
 
 export default AddWorkout;
