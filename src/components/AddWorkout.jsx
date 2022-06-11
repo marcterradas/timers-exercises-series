52876b509e8b84fb3e22f560e7abc39e01bffc5b
marcterradas <terradas.marc@gmail.com>
2022-06-11 16:56:58 +0200

    feat: :sparkles: Exercice, add and cancel buttons

--- src/components/AddWorkout.jsx
+++ src/components/AddWorkout.jsx
@@ -3,6 +3,8 @@ import { Text, TouchableOpacity, View, TextInput } from "react-native";
 import IonIcon from "react-native-vector-icons/Ionicons";
 import { t } from "i18next";
 
+import ActionButton from "./ActionButton";
+
 import styles from "../styles/workout.styles";
 
 const AddWorkout = () => {
@@ -14,13 +16,21 @@ const AddWorkout = () => {
     color: "black",
   };
 
-  function addExercice() {
+  function fnShowForm() {
     setShowForm(true);
+  }
+
+  function showButton() {
+    setShowForm(false);
+  }
+
+  function saveExercice() {
+    console.log("save exercice");
   }
 
   const Button = () => {
     return (
-      <TouchableOpacity onPress={addExercice} style={styles.bigButton}>
+      <TouchableOpacity onPress={fnShowForm} style={styles.bigButton}>
         <IonIcon name={icon.name} size={icon.size} color={icon.color} />
         <Text style={styles.buttonText}>{t("add")}</Text>
       </TouchableOpacity>
@@ -37,6 +47,18 @@ const AddWorkout = () => {
         <View style={styles.containerJustifyLeft}>
           <Text style={styles.label}>{t("number_repetitions")}</Text>
           <TextInput keyboardType="numeric" style={styles.input} />
+        </View>
+        <View style={styles.actionContainer}>
+          <ActionButton
+            label={t("cancel")}
+            type="backButton"
+            method={showButton}
+          />
+          <ActionButton
+            label={t("save")}
+            type="actionButton"
+            method={saveExercice}
+          />
         </View>
       </View>
     );
