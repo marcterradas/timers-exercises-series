c279a9eb609179ca1d823bd0b4721ce98e7f5e70
marcterradas <terradas.marc@gmail.com>
2022-06-17 15:26:20 +0200

    feat: :sparkles: create component add exercice

---
+++ src/components/AddExercice.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+import IonIcon from "react-native-vector-icons/Ionicons";
+import { t } from "i18next";
+
+import styles from "../styles/workout.styles";
+
+const AddExercice = (callBack) => {
+  const icon = {
+    name: "add-circle-outline",
+    size: 40,
+    color: "black",
+  };
+
+  return (
+    <TouchableOpacity onPress={callBack} style={styles.bigButton}>
+      <IonIcon name={icon.name} size={icon.size} color={icon.color} />
+      <Text style={styles.buttonText}>{t("add")}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default AddExercice;
