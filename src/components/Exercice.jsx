c4a3d65c858c33432d36d2c50c2d5614b57a4f47
marcterradas <terradas.marc@gmail.com>
2022-06-20 07:03:08 +0200

    feat: :sparkles: Exercice component, init styles

--- src/components/Exercice.jsx
+++ src/components/Exercice.jsx
@@ -1,9 +1,11 @@
 import { React } from "react";
 import { View, Text } from "react-native";
 
+import styles from "../styles/workout.styles";
+
 const Exercice = ({ name, repetitions, breakTime }) => {
   return (
-    <View>
+    <View style={styles.exerciceButton}>
       <Text>{name}</Text>
       <Text>{repetitions}</Text>
       <Text>{breakTime}</Text>
