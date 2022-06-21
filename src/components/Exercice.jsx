44a94a4d9b6604d918ad28616fa86aed08492eff
marcterradas <terradas.marc@gmail.com>
2022-06-21 16:53:21 +0200

    feat: :sparkles: update labels

--- src/components/Exercice.jsx
+++ src/components/Exercice.jsx
@@ -1,5 +1,6 @@
 import { React } from "react";
 import { View, Text } from "react-native";
+import { t } from "i18next";
 
 import styles from "../styles/workout.styles";
 
@@ -7,8 +8,8 @@ const Exercice = ({ name, repetitions, breakTime }) => {
   return (
     <View style={styles.exerciceButton}>
       <Text style={styles.exerciceTitle}>{name}</Text>
-      <Text>{repetitions}</Text>
-      <Text>{breakTime}</Text>
+      <Text>{t("exercice_number_repetitions", { number: repetitions })}</Text>
+      <Text>{t("exercice_break_time", { number: breakTime })}</Text>
     </View>
   );
 };
