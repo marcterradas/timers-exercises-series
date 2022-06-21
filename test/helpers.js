eb23ea3177b65277f88eb7ae506bb8db939433d7
marcterradas <terradas.marc@gmail.com>
2022-06-21 19:37:02 +0200

    test: :sparkles: add cleanString test

--- test/helpers.js
+++ test/helpers.js
@@ -3,4 +3,13 @@ import { cleanString, cleanNumber } from "../src/logic/helpers";
 
 test("cleanString", () => {
   expect(cleanString()).toBe("");
+  expect(cleanString(null)).toBe("");
+  expect(cleanString(false)).toBe("");
+  expect(cleanString(undefined)).toBe("");
+  expect(cleanString("")).toBe("");
+  expect(cleanString("s")).toBe("S");
+  expect(cleanString("string")).toBe("String");
+  expect(cleanString("String")).toBe("String");
+  expect(cleanString(" string ")).toBe("String");
+  expect(cleanString(" String ")).toBe("String");
 });
