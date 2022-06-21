b2259dfa11e4fba2c5b254d92a1a2457d6edcfe1
marcterradas <terradas.marc@gmail.com>
2022-06-21 19:36:21 +0200

    feat: :sparkles: improve cleanString function

--- src/logic/helpers.js
+++ src/logic/helpers.js
@@ -1,4 +1,8 @@
 export function cleanString(string = "") {
+  if (!string) {
+    string = "";
+  }
+  string = string.trim();
   const firstLetter = string.trim().charAt(0).toUpperCase();
   const restOfString = string.slice(1);
   return `${firstLetter}${restOfString}`;
