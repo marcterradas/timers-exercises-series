ac3fdb180546a0a93275ee6b81568c65188b0660
marcterradas <terradas.marc@gmail.com>
2022-06-22 16:32:49 +0200

    feat: :sparkles: update exercice button, add callback param and refactor view to touch

--- src/components/Exercice.jsx
+++ src/components/Exercice.jsx
@@ -1,16 +1,16 @@
 import { React } from "react";
-import { View, Text } from "react-native";
+import { TouchableOpacity, Text } from "react-native";
 import { t } from "i18next";
 
 import styles from "../styles/workout.styles";
 
-const Exercice = ({ name, repetitions, breakTime }) => {
+const Exercice = ({ name, repetitions, breakTime, callBack }) => {
   return (
-    <View style={styles.exerciceButton}>
+    <TouchableOpacity onPress={() => callBack()} style={styles.exerciceButton}>
       <Text style={styles.exerciceTitle}>{name}</Text>
       <Text>{t("exercice_number_repetitions", { number: repetitions })}</Text>
       <Text>{t("exercice_break_time", { number: breakTime })}</Text>
-    </View>
+    </TouchableOpacity>
   );
 };
 
