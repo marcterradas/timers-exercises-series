8ba0520bafb510604b2580f5b66b67cd173088d5
marcterradas <terradas.marc@gmail.com>
2022-06-24 13:48:48 +0200

    feat: :sparkles: init popup component

---
+++ src/components/PopupExercice.jsx
@@ -0,0 +1,12 @@
+import { React } from "react";
+import { View, Text } from "react-native";
+
+const PopupExercice = () => {
+  return (
+    <View>
+      <Text>Popup exercice ...</Text>
+    </View>
+  );
+};
+
+export default PopupExercice;
