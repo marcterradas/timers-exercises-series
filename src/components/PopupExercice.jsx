9ba765f6c46cf7decd189c37389926a55e062f1b
marcterradas <terradas.marc@gmail.com>
2022-06-30 21:24:36 +0200

    feat: :sparkles: popup, init structure

--- src/components/PopupExercice.jsx
+++ src/components/PopupExercice.jsx
@@ -1,11 +1,20 @@
 import { React } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
+
+import styles from "../styles/workout.styles";
 
 const PopupExercice = ({ name, repetitions, breakTime }) => {
   return (
-    <View>
-      <Text>Popup exercice ...</Text>
-    </View>
+    <>
+      <ScrollView style={styles.subContainer}>
+        <View style={styles.container}>
+          <Text>Popup exercice ...</Text>
+        </View>
+      </ScrollView>
+      <View style={styles.actionContainer}>
+        <Text>Buttons ...</Text>
+      </View>
+    </>
   );
 };
 
