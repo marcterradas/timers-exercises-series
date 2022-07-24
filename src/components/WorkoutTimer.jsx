7fc32d612f135569e299a2c8d4be576f8dfcfefb
marcterradas <terradas.marc@gmail.com>
2022-07-24 18:52:06 +0200

    feat: :sparkles: create component workout timer

---
+++ src/components/WorkoutTimer.jsx
@@ -0,0 +1,13 @@
+import { React } from "react";
+import { View, Text } from "react-native";
+
+const WorkoutTimer = (workout) => {
+  console.log(workout);
+  return (
+    <View>
+      <Text>todo workout timer element ...</Text>
+    </View>
+  );
+};
+
+export default WorkoutTimer;
