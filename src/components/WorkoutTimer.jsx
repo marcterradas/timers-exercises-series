f19cf25b3170d466b469c95125d87db5020f43d9
marcterradas <terradas.marc@gmail.com>
2022-08-17 15:39:54 +0200

    timer create function getNextExercice

--- src/components/WorkoutTimer.jsx
+++ src/components/WorkoutTimer.jsx
@@ -1,4 +1,4 @@
-import { React } from "react";
+import { React, useState, useEffect } from "react";
 import { ScrollView, View, Text } from "react-native";
 import { useTranslation } from "react-i18next";
 
@@ -9,6 +9,20 @@ import ActionButton from "./ActionButton";
 const WorkoutTimer = ({ workout, callBack }) => {
   const { t } = useTranslation();
   const [name, exercices] = workout;
+  const [currentExercice, setCurrentExercice] = useState(false);
+
+  function getNextExercice() {
+    const exercice = exercices.shift();
+
+    if (typeof exercice === "undefined") {
+      callBack();
+      return false;
+    }
+
+    setCurrentExercice(exercice);
+  }
+
+  useEffect(getNextExercice, []);
 
   return (
     <>
