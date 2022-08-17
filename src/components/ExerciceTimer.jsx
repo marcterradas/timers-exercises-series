ce724fcd773dc8b8fb133781cf4bec9204a4fb7f
marcterradas <terradas.marc@gmail.com>
2022-08-17 17:54:27 +0200

    feat: :sparkles: create exercice timer component

---
+++ src/components/ExerciceTimer.jsx
@@ -0,0 +1,12 @@
+import { React } from "react";
+import { View, Text } from "react-native";
+
+const ExerciceTimer = ({ exercice }) => {
+  return (
+    <View>
+      <Text>Exercice timer ...</Text>
+    </View>
+  );
+};
+
+export default ExerciceTimer;
