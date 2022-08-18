e76a84a56b402ee47dd3454225eaa227bc78ab85
marcterradas <terradas.marc@gmail.com>
2022-08-18 12:19:24 +0200

    test: :sparkles: minutes to seconds add default test

--- test/timer.js
+++ test/timer.js
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { secondsToMinutes } from "../src/logic/timer";
+import { secondsToMinutes, minutesToSeconds } from "../src/logic/timer";
 
 test("secondsToMinutes", () => {
   expect(secondsToMinutes()).toBe("00:00");
@@ -11,4 +11,11 @@ test("secondsToMinutes", () => {
   expect(secondsToMinutes(90)).toBe("01:30");
   expect(secondsToMinutes(30)).toBe("00:30");
   expect(secondsToMinutes(59)).toBe("00:59");
+});
+
+test("minutesToSeconds", () => {
+  expect(minutesToSeconds()).toBe(0);
+  expect(minutesToSeconds(null)).toBe(0);
+  expect(minutesToSeconds(false)).toBe(0);
+  expect(minutesToSeconds(undefined)).toBe(0);
 });
