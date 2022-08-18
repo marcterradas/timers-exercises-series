481f3dbeaf971a81dac66305a0d4488b2a1d313d
marcterradas <terradas.marc@gmail.com>
2022-08-18 11:55:08 +0200

    test: :sparkles: add final test

--- test/timer.js
+++ test/timer.js
@@ -7,4 +7,8 @@ test("secondsToMinutes", () => {
   expect(secondsToMinutes(false)).toBe("00:00");
   expect(secondsToMinutes(undefined)).toBe("00:00");
   expect(secondsToMinutes(0)).toBe("00:00");
+  expect(secondsToMinutes(60)).toBe("01:00");
+  expect(secondsToMinutes(90)).toBe("01:30");
+  expect(secondsToMinutes(30)).toBe("00:30");
+  expect(secondsToMinutes(59)).toBe("00:59");
 });
