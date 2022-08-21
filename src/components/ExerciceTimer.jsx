03ea54264f2ca91d1c1c3590cbe3a5175d48739d
marcterradas <terradas.marc@gmail.com>
2022-08-21 16:10:18 +0200

    feat: :sparkles: exercice, show time

--- src/components/ExerciceTimer.jsx
+++ src/components/ExerciceTimer.jsx
@@ -1,12 +1,18 @@
-import { React } from "react";
+import { React, useState } from "react";
 import { View, Text } from "react-native";
+
+import {
+  secondsToMinutes,
+  minutesToSeconds,
+  substractOneSecond,
+} from "../logic/timer.js";
 
 const ExerciceTimer = ({ exercice = {} }) => {
   const { name, repetitions, breakTime } = exercice;
-  console.log({ name, repetitions, breakTime });
+  const [time, setStime] = useState(secondsToMinutes(breakTime));
   return (
     <View>
-      <Text>Exercice timer ...</Text>
+      <Text>{time}</Text>
     </View>
   );
 };
